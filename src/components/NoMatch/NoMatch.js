31d21c5db764748388991118f893b0669a0f9572
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2020-10-07 21:29:01 -0400

    all tasks are done

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,6 @@
+import React from "react";
+const noMatch = () => (
+  <h1 style={{textAlign: "center"}}>404 Page mot found</h1>
+)
+
+export default noMatch;
---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Users from "../Users/Users"
+import Courses from '../Courses/Courses'
+
+class Layout extends Component {
+
+}
+
+export default Layout;
